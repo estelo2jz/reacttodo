c4511e623e2d17cecb0dd35fec46214201773efc
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-10-09 10:27:24 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Logo1 from '../src/logo/logo1.png';
-import './App.css';
+import './App.scss';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <img src={Logo1} />
-        <h1>To Do's</h1>
+    <div className="todo-outer-app">
+      <div className="todo-app">
+        <div>
+          <img src={Logo1} />
+        </div>
+        <TodoList />
       </div>
     </div>
   );
